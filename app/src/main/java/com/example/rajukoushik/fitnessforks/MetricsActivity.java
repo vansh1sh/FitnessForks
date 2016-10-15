f729bdb143a50d85724a0846cb817b8de48577a0
rajukoushik <g.rajukoushik@gmail.com>
2016-10-16 03:34:39 +0530

    Updated Metrics Activity.

--- app/src/main/java/com/example/rajukoushik/fitnessforks/MetricsActivity.java
+++ app/src/main/java/com/example/rajukoushik/fitnessforks/MetricsActivity.java
@@ -1,7 +1,9 @@
 package com.example.rajukoushik.fitnessforks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MetricsActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MetricsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metrics);
+    }
+
+    public void clickingNext(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/rajukoushik/fitnessforks/RegisterActivity.java
+++ app/src/main/java/com/example/rajukoushik/fitnessforks/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.example.rajukoushik.fitnessforks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void clickNext(View view) {
+        Intent intent = new Intent(this, MetricsActivity.class);
+        startActivity(intent);
     }
 }
