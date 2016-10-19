a3a90e82b2e3cf939e4691b676d49c7e6ad3e925
rajukoushik <g.rajukoushik@gmail.com>
2016-10-19 09:13:01 +0530

    Updated drawer and menu items.

--- app/src/main/java/com/example/rajukoushik/fitnessforks/HomeActivity.java
+++ app/src/main/java/com/example/rajukoushik/fitnessforks/HomeActivity.java
@@ -100,14 +100,6 @@ public class HomeActivity extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
