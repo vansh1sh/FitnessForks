c2125814f44b945d4c6c43440e1917b6a491817e
rajukoushik <g.rajukoushik@gmail.com>
2016-10-20 17:04:46 +0530

    Updated Complete Login and Registration .

--- app/src/main/java/com/example/rajukoushik/fitnessforks/GoalsActivity.java
+++ app/src/main/java/com/example/rajukoushik/fitnessforks/GoalsActivity.java
@@ -1,9 +1,13 @@
 package com.example.rajukoushik.fitnessforks;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
 public class GoalsActivity extends AppCompatActivity {
 
@@ -11,6 +15,82 @@ public class GoalsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goals);
+
+
+        final ImageView image = (ImageView) findViewById(R.id.muscle);
+        image.setOnTouchListener(new View.OnTouchListener() {
+            private Rect rect;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    image.setColorFilter(Color.argb(50, 0, 0, 0));
+                    rect = new Rect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    image.setColorFilter(Color.argb(0, 0, 0, 0));
+                }
+                if(event.getAction() == MotionEvent.ACTION_MOVE){
+                    if(!rect.contains(v.getLeft() + (int) event.getX(), v.getTop() + (int) event.getY())){
+                        image.setColorFilter(Color.argb(0, 0, 0, 0));
+                    }
+                }
+                Intent intent = new Intent(GoalsActivity.this, HomeActivity.class);
+                startActivity(intent);
+
+                return false;
+            }
+        });
+
+        final ImageView image1 = (ImageView) findViewById(R.id.loseweigt);
+        image.setOnTouchListener(new View.OnTouchListener() {
+            private Rect rect;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    image.setColorFilter(Color.argb(50, 0, 0, 0));
+                    rect = new Rect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    image.setColorFilter(Color.argb(0, 0, 0, 0));
+                }
+                if(event.getAction() == MotionEvent.ACTION_MOVE){
+                    if(!rect.contains(v.getLeft() + (int) event.getX(), v.getTop() + (int) event.getY())){
+                        image.setColorFilter(Color.argb(0, 0, 0, 0));
+                    }
+                }
+                Intent intent = new Intent(GoalsActivity.this, HomeActivity.class);
+                startActivity(intent);
+                return false;
+            }
+        });
+
+        final ImageView image2 = (ImageView) findViewById(R.id.maintain);
+        image.setOnTouchListener(new View.OnTouchListener() {
+            private Rect rect;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    image.setColorFilter(Color.argb(50, 0, 0, 0));
+                    rect = new Rect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    image.setColorFilter(Color.argb(0, 0, 0, 0));
+                }
+                if(event.getAction() == MotionEvent.ACTION_MOVE){
+                    if(!rect.contains(v.getLeft() + (int) event.getX(), v.getTop() + (int) event.getY())){
+                        image.setColorFilter(Color.argb(0, 0, 0, 0));
+                    }
+                }
+                Intent intent = new Intent(GoalsActivity.this, HomeActivity.class);
+                startActivity(intent);
+                return false;
+            }
+        });
     }
 
     public void btnNextGoals(View view) {
---
+++ app/src/main/java/com/example/rajukoushik/fitnessforks/PrefManger.java
@@ -0,0 +1,49 @@
+package com.example.rajukoushik.fitnessforks;
+
+/**
+ * Created by rajukoushik on 20/10/16.
+ */
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+
+public class PrefManger {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "androidhive-welcome";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManger(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+    public void setToken(String token)
+    {
+        editor.putString("token", token);
+    }
+
+    public String getToken()
+    {
+        return pref.getString("token", "");
+    }
+
+}
